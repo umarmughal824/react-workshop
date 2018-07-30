b317b4c2ee366d3954f398b9f1168e03fb51fa71
Umar <umar.asghar@arbisoft.com>
2018-07-30 18:57:27 +0500

    add show and hide details panel

--- src/modules/Lessons/7_LessonContainerPresentational/components/ResultsList.js
+++ src/modules/Lessons/7_LessonContainerPresentational/components/ResultsList.js
@@ -1,35 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-const ResultsList = ({results}) => {	
-  return (
-  	<table className="flights-list">
-		<thead>
-			<tr>
-				<th>Airline</th>
-				<th>Departure</th>
-				<th>Arrival</th>
-				<th>Duration</th>
-				<th>Stops</th>
-				<th>Price</th>
-			</tr>
-		</thead>
-		<tbody>
-		{
-			results.map((flight, index) => 
-				<tr key={index}>
-					<td>{flight.airline}</td>
-					<td>{flight.details.departure}</td>
-					<td>{flight.details.arrival}</td>
-					<td>{flight.duration}</td>
-					<td>{flight.stops}</td>
-					<td>Rs. <strong>{flight.priceDetails.totalFare}</strong></td>
-				</tr>
-			)
-		}
-		</tbody>
-	</table>
-  );
+import ResultDetail from './ResultDetail';
+
+class ResultsList extends Component{
+	constructor(props){
+		super(props);
+
+		this.state = {
+			results: props.results,
+		};
+	}
+
+	render(){
+		const { results, isHidden } = this.state;
+	  
+	  return (
+			<div>
+			{
+				results.map((flight, index) => 
+					<div key={index}>
+						<ResultDetail flight={flight} />
+					</div>
+				)
+			}
+			</div>
+	  );
+	}
 }
 
 ResultsList.propTypes = {
